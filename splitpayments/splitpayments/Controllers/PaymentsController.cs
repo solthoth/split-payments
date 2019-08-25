33712f657f7b4918c5b4266d1c83955e5b09df65
Carlos Barajas <soldotsol@gmail.com>
2019-08-24 19:37:04 -0700

    good work

---
+++ splitpayments/splitpayments/Controllers/PaymentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace splitpayments.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        public IActionResult Get(string id)
+        {
+
+            return Ok(true);
+
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Payment payment)
+        {
+
+            return Ok();
+
+        }
+    }
+}
