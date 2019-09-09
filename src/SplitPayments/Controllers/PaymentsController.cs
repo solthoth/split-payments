24fb73c3edc6615744ccef4829745a3c7fb5f262
Carlos Barajas <soldotsol@gmail.com>
2019-09-08 18:12:48 -0700

    Project files renamed and reorganized directory structure

--- src/SplitPayments/Controllers/PaymentsController.cs
+++ src/SplitPayments/Controllers/PaymentsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace splitpayments.Controllers
+namespace SplitPayments.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
